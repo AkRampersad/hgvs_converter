b7ebcb0b08f103dbf0c9b4851f4272ecc2177213
Akash Rampersad <93301335+AkRampersad@users.noreply.github.com>
2023-10-11 15:26:25 -0400

    Update converter.py
    added hgvs_parser function so that a variant dictionary is produced.
    
    Variant dictionary contains transcript name, pos, ref, alt, and chromosome number 
    
    Still only works for RefSeq/GenBank transcript IDs

--- converter.py
+++ converter.py
@@ -1,6 +1,8 @@
 from Bio import Entrez, SeqIO
+import hgvs.parser
 
 
+var_example = "NM_001366781.1:c.90T>C"
 def obtain_chromosome(transcript_id):
     Entrez.email = "akashjrampersad@gmail.com"
 
@@ -13,6 +15,31 @@ def obtain_chromosome(transcript_id):
             return chromosome
 
     return None
-print(obtain_chromosome("NM_001005405.2"))
+
+def hgvs_parser(hgvs_code):
+
+    hp = hgvs.parser.Parser()
+    var_dict = {"transcript": None,
+                "pos": None,
+                "ref" : None,
+                "alt" : None,
+                'chrom' : None
+                }
+
+    var_g = hp.parse_hgvs_variant(hgvs_code)
+    var_dict["transcript"] = var_g.ac
+    var_dict["pos"] = var_g.posedit.pos.start.base
+    var_dict["ref"] = var_g.posedit.edit.ref
+    var_dict["alt"] = var_g.posedit.edit.alt
+    var_dict["chrom"] = obtain_chromosome(var_g.ac)
+    return var_dict
+
+print(hgvs_parser(var_example))
+
+
+
+
+
+
 
 
